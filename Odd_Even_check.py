0b2cb6d31506a2d23dd7a239370ad4e80de1d70f
Deepak-Dubey12 <165334532+Deepak-Dubey12@users.noreply.github.com>
2025-12-08 12:33:09 +0000

    Add files via upload
    In this program I am taking an input from user and just checking that given number is ODD or EVEN.

---
+++ Odd_Even_check.py
@@ -0,0 +1,8 @@
+# Enter Number is Even or Odd
+
+number = int(input("Enter the number you want to check := "))
+
+if(number % 2 == 0):
+    print("The Given Number Is Even")
+else:
+    print("The Given Number Is Odd")
